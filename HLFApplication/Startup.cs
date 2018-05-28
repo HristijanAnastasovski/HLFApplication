7f008f702f7eb8bb150ed260b31a897f92e7db56
Hristijan <anastasovski.h@gmail.com>
2018-05-28 17:04:27 +0200

    Initial commit

---
+++ HLFApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HLFApplication.Startup))]
+namespace HLFApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
